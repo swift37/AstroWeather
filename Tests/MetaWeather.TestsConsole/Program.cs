8c23f23b96d2c7e9784f9a92ee31dcba659fc450
Swift <swiftx37@gmail.com>
2023-10-23 17:28:18 +0200

    Console tests project setup.

--- Tests/MetaWeather.TestsConsole/Program.cs
+++ Tests/MetaWeather.TestsConsole/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    private static IHost _Hosting;
+
+    public static IHost Hosting => _Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+    public static IServiceProvider Services => Hosting.Services;
+
+    public static IHostBuilder CreateHostBuilder(string[] args) => Host
+        .CreateDefaultBuilder()
+        .ConfigureServices(ConfigureServices);
+
+    private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+    {
+        
+    }
+       
+    public static async Task Main(string[] args)
+    {
+        using var host = Hosting;
+        await host.StartAsync();
+
+        Console.WriteLine("Completed!");
+        Console.ReadLine();
+
+        await host.StopAsync();
+    }
+}
